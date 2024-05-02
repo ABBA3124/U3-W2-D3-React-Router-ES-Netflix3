90726b7b383d0af302832c9196c1537fe6a9db13
ABBA3124 <davideabbadessa73@gmail.com>
2024-05-02 15:39:58 +0200

    implementazione di NavLink con il metodo to="" gestione onClick per il navigate + implementazione Tv Shows

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import React, { useState } from "react"
 import Footer from "./components/Footer/Footer.jsx"
 import CustomNavbar from "./components/Navbar/CustomNavbar.jsx"
 import MovieGallery from "./components/MovieGallery/MovieGallery.jsx"
@@ -8,37 +7,40 @@ import FilterGenres from "./components/FilterGenres/FilterGenres.jsx"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import ProfilePage from "./components/ProfilePage/ProfilePage.jsx"
 import SettingsPage from "./components/SettingsPage/SettingsPage.jsx"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import  TVShowsPage  from "./components/TVShowsPage/TVShowsPage.jsx"
 
 function App() {
-  // Stato per gestire la vista attiva
-  const [activeView, setActiveView] = useState("home")
-
-  // Funzione per cambiare la vista
-  const changeView = (view) => {
-    setActiveView(view)
-  }
-
   return (
-    <div className="App bg-black">
-      <main className="bg-black mx-5">
-      <CustomNavbar changeView={changeView} />
-        {activeView === "home" && (
-          <>
-            <FilterGenres />
-            <MovieGallery title="action" categories="Titoli consigliati" />
-            <MovieGallery title="transformers" categories="Titoli che potrebbero piacerti" />
-            <MovieGallery title="Harry Potter" categories="Trending Now" />
-            <MovieGallery title="fast and furious" categories="Perché hai guardato Need For Speed" />
-            <MovieGallery title="Lord of the Rings" categories="Watch It Again" />
-            <MovieGallery title="Star Wars" categories="New Releases" />
-            <MovieGallery title="dkhbfjhsbhdf" categories="Titoli consigliati" /> {/* lasciato appositamente per far vedere che se non trova riscontro ci avvisa */}
-          </>
-        )}
-        {activeView === "settings" && <SettingsPage />}
-        {activeView === "profile" && <ProfilePage />}
-      <Footer />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App bg-black">
+        <main className="bg-black mx-5">
+          <CustomNavbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <FilterGenres />
+                  <MovieGallery title="action" categories="Titoli consigliati" />
+                  <MovieGallery title="transformers" categories="Titoli che potrebbero piacerti" />
+                  <MovieGallery title="Harry Potter" categories="Trending Now" />
+                  <MovieGallery title="fast and furious" categories="Perché hai guardato Need For Speed" />
+                  <MovieGallery title="Lord of the Rings" categories="Watch It Again" />
+                  <MovieGallery title="Star Wars" categories="New Releases" />
+                  <MovieGallery title="dkhbfjhsbhdf" categories="Titoli consigliati" />{" "}
+                  {/* lasciato appositamente per far vedere che se non trova riscontro ci avvisa */}
+                </>
+              }
+            />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/Shows" element={<TVShowsPage />} />
+          </Routes>
+          <Footer />
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/TVShowsPage/TVShowsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+function TVShowsPage () {
+    return (
+        <div className="text-center mt-5">
+            <h1 className="text-info">Tv Shows Page</h1>
+            <p className="text-info fs-3 mt-5 mb-5">Al momento vuota</p>
+        </div>
+    )
+}
+
+export default TVShowsPage
